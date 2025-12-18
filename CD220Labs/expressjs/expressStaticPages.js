25e77f29fb7813a703e6695676fd174ae2fa629c
Mohamed Hamed <mhmd.h.hamed@gmail.com>
2025-12-18 06:01:48 +0100

    relative path to the script

--- CD220Labs/expressjs/expressStaticPages.js
+++ CD220Labs/expressjs/expressStaticPages.js
@@ -1,13 +1,14 @@
- // Import the Express.js library
-const express = require('express');
+// Import the Express.js library
+const express = require("express");
+const path = require("path");
 
 // Create an instance of an Express application
 const app = new express();
 
-// Serve static files from the 'cad220_staticfiles' directory
-app.use(express.static('cad220_staticfiles'));
+// Serve static files from the 'cad220_staticfiles' directory (relative to this file)
+app.use(express.static(path.join(__dirname, "cad220_staticfiles")));
 
 // Start the server and listen on port 3333
 app.listen(3333, () => {
-    console.log(`Listening at http://localhost:3333`);
+  console.log(`Listening at http://localhost:3333`);
 });
